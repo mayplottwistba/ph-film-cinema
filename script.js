04f79d636275d0a0846c8d407f43dec625004c31
mayplottwistba <vergeljoseph.g.m@gmail.com>
2026-08-21 10:51:01 +0800

    room boy

--- script.js
+++ script.js
@@ -14,6 +14,13 @@ const platformNames = {
 const whatsNew = [
   {
     date: "08.21.26",
+    version: "2.5.2",
+    updates: [
+      "A non-HD version of <b><i>Room Boy (2005)</i></b> is found on YouTube.", 
+    ],
+  },
+  {
+    date: "08.21.26",
     version: "2.5",
     updates: [
       "Download links for <b><i>Niño (2011) </i></b>, <b><i>Porno (2013)</i></b>, <b><i>Children's Show (2014)</i></b>, <b><i>Brutus (2008)</i></b>, <b><i>Rotonda (2006)</i></b>, <b><i>Gulong (2007)</i></b>, <b><i>Ligaw Liham (2007)</i></b>, <b><i>In da Red Corner (2006)</i></b>, <b><i>Sigwa (2010)</i></b>, and <b><i>Maria (2023)</i></b>." 
