c9af52c7970624c495a9985772fbb9c77014876a
mayplottwistba <vergeljoseph.g.m@gmail.com>
2026-08-21 10:52:36 +0800

    edited update

--- script.js
+++ script.js
@@ -23,7 +23,7 @@ const whatsNew = [
     date: "08.21.26",
     version: "2.5",
     updates: [
-      "Download links for <b><i>Niño (2011) </i></b>, <b><i>Porno (2013)</i></b>, <b><i>Children's Show (2014)</i></b>, <b><i>Brutus (2008)</i></b>, <b><i>Rotonda (2006)</i></b>, <b><i>Gulong (2007)</i></b>, <b><i>Ligaw Liham (2007)</i></b>, <b><i>In da Red Corner (2006)</i></b>, <b><i>Sigwa (2010)</i></b>, and <b><i>Maria (2023)</i></b>." 
+      "Added download links for <b><i>Niño (2011) </i></b>, <b><i>Porno (2013)</i></b>, <b><i>Children's Show (2014)</i></b>, <b><i>Brutus (2008)</i></b>, <b><i>Rotonda (2006)</i></b>, <b><i>Gulong (2007)</i></b>, <b><i>Ligaw Liham (2007)</i></b>, <b><i>In da Red Corner (2006)</i></b>, <b><i>Sigwa (2010)</i></b>, and <b><i>Maria (2023)</i></b>." 
     ],
   },
   {
